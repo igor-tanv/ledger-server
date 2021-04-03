ff12856700959fd6db037fc6fe931b658e44548c
Igor Tatarinov <igor.tanv@gmail.com>
2021-04-03 17:31:00 +0700

    update getLedger

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,14 @@
 const LedgerData = require('../db/data/LedgerData')
 
 const getLedger = async () => {
-  return await LedgerData.getLedger().map((row) => {
+  const ledger = await LedgerData.getLedger()
+  console.log(ledger, 5)
+  const newLedger = ledger.map((row) => {
     if (row['id'] !== undefined) { row['id'] = row['id'].toString() }
     return row
   })
+  console.log(newLedger, 10)
+  return newLedger
 }
 
 const postLedger = async (data) => {
