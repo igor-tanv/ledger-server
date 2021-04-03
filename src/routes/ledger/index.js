1940457a7d98de5dc69037d4fe742661e5f6b30d
Igor Tatarinov <igor.tanv@gmail.com>
2021-04-03 17:44:08 +0700

    remove unused files and directories

--- src/routes/ledger/index.js
+++ src/routes/ledger/index.js
@@ -5,12 +5,10 @@ const router = new express.Router()
 
 router.get('/api', async (req, res) => {
   const ledger = await LedgerService.getLedger()
-  console.log(ledger, 8)
   res.status(200).json(ledger.sort((a, b) => b.purchaseDate - a.purchaseDate))
 })
 
 router.post('/api/ledger', async (req, res) => {
-  console.log(req.body, 18)
   res.status(200).json(LedgerService.postLedger(req.body))
 })
 
