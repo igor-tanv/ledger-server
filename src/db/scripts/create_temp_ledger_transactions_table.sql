7c436cfc265c7971cae6b1bd89dfcacbd366cfc8
Igor Tatarinov <igor.tanv@gmail.com>
2021-07-17 15:26:40 +0700

    update sql syntax

--- src/db/scripts/create_temp_ledger_transactions_table.sql
+++ src/db/scripts/create_temp_ledger_transactions_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `temp_ledger_transaction` (
   id  BINARY(36) NOT NULL PRIMARY KEY,
-  ledger_id BINARY(36) REFERENCES temp_ledger(id)
+  ledger_id BINARY(36) REFERENCES temp_ledger(id),
   user varchar(255) NOT NULL,
   item varchar(255) NOT NULL,
-  cost INT NOT NULL,
+  cost INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
